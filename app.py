4de6b7ccd5932c2775216b707098844e1c03ffe8
cihat yıldız <cihatyldz1907@gmail.com>
2023-05-03 16:56:53 +0300

    Relationship

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+
 
 
 def create_app(db_url=None):
